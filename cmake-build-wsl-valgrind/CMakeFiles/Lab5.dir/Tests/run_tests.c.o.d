9b1f76a4bd1d94bda8eeaed4d9d2772cd52173f0
OulGabrielAlin <alyngaby05@gmail.com>
2025-03-25 00:24:21 +0200

    Trying to Merge Master with this branch

--- cmake-build-wsl-valgrind/CMakeFiles/Lab5.dir/Tests/run_tests.c.o.d
+++
@@ -1,7 +0,0 @@
-CMakeFiles/Lab5.dir/Tests/run_tests.c.o: \
- /mnt/d/Info_Facultate/Semestrul_2/OOP/Lab5/Tests/run_tests.c \
- /usr/include/stdc-predef.h \
- /mnt/d/Info_Facultate/Semestrul_2/OOP/Lab5/Tests/teste_domain.h \
- /mnt/d/Info_Facultate/Semestrul_2/OOP/Lab5/Tests/teste_list.h \
- /mnt/d/Info_Facultate/Semestrul_2/OOP/Lab5/Tests/teste_repo.h \
- /mnt/d/Info_Facultate/Semestrul_2/OOP/Lab5/Tests/teste_service.h
